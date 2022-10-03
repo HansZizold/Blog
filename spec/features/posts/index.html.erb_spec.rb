c21cd64879d1e9ba69f9a673360dc7efe6fc4537
emmiiorji <emmanuelorji2013@gmail.com>
2022-10-03 23:40:12 +0100

    Create models in user index test file

--- spec/features/posts/index.html.erb_spec.rb
+++ spec/features/posts/index.html.erb_spec.rb
@@ -1,10 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :feature do
-  describe 'Index page' do
-    it 'shows the right content' do
-      visit users_path
-      expect(page).to have_content('Number of posts')
-    end
+RSpec.describe 'Post Index', type: :feature do
+  before :each do
+    @user = User.create(
+      name: 'Gaby',
+      photo: 'bulma.jpg',
+      bio: 'Systems Engineering Student'
+    )
+    user2 = User.create(
+      name: 'Leo',
+      photo: 'vegeta.jpg',
+      bio: 'Law Student'
+    )
+    @post1 = Post.create(author: @user, title: 'Post 1', text: 'Post One')
+    post2 = Post.create(author: @user, title: 'Post 2', text: 'Post Two')
+    post3 = Post.create(author: @user, title: 'Post 3', text: 'Post Three')
+    post4 = Post.create(author: @user, title: 'Post 4', text: 'Post Four')
+
+    comment1 = Comment.create(author: user1, post: post3, text: 'Hi Tom1')
+    comment2 = Comment.create(author: user2, post: post3, text: 'Hi Tom2')
+    comment3 = Comment.create(author: user3, post: post3, text: 'Hi Tom3')
+    @first_comments = [comment1, comment2, comment3]
+    visit posts_path(@user.id)
   end
 end
